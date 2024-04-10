0b5055f89d50c9d5ebf95b3dc3cecf6ac15dbe00
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-04-10 14:46:33 +0100

    Auth Implementation Amplify and Cognito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
