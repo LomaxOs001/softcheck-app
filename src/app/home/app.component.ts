56a48911a0ffe9a977cf14ca963e8a4a6352a4a5
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-04-17 22:00:57 +0100

    Refining and refactoring

--- src/app/home/app.component.ts
+++ src/app/home/app.component.ts
@@ -6,8 +6,10 @@ import { Hub } from 'aws-amplify/utils';
 import { DataManagementComponent } from '../data.management/data.management.component';
 import { Router } from '@angular/router';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
+import { MyLocalStorage } from '../services/myLocalStorage';
 
 import awsconfig from '../../aws-exports';
+import { DataprocessingService } from '../services/dataprocessing.service';
 
 Amplify.configure(awsconfig);
 
@@ -21,18 +23,24 @@ Amplify.configure(awsconfig);
 export class AppComponent implements OnInit {
   title = 'finchain-web-app';
 
-  constructor(private router: Router, private auth: AuthenticatorService) {}
+  constructor(private router: Router, private auth: AuthenticatorService, private session: MyLocalStorage, private dp: DataprocessingService) {}
 
   ngOnInit() {
     Hub.listen('auth', (data) => {
       const { payload } = data;
-      console.log('A new auth event has happened: ', data);
+      
+      console.log('A new auth event has happened: ', data.payload);
+
       if (payload.event === 'signedIn') {
-        this.router.navigate(['/datamanager']);
-        console.log('User logged in');
+        this.router.navigate(['/datamanage']);
+        this.session.setToken();
+        this.dp.setAuthzUsername(payload.data.username);
+      }
+      if (payload.event === 'signedOut') {
+        this.router.navigate(['/']);
+        this.session.clear();
       }
     });
   }
-
-
-}
+  }
+  
