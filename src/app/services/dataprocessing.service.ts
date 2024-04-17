c9437eaefe0b17c86b0a79ee0c78932ff324e55e
Lomax Osomba <72916140+LomaxOS00243@users.noreply.github.com>
2024-04-17 10:02:07 +0100

    Update from github dataprocessing.service.ts

--- src/app/services/dataprocessing.service.ts
+++ src/app/services/dataprocessing.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpRequest, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
