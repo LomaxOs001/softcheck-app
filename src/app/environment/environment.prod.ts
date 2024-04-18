a55049ccdb6f5b5ec0038b7e5ee630a04d35520b
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-04-18 07:11:24 +0100

    dev url update

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    finchainAPI: {
+        endpoint:
+          'https://u7jg5lpkgb.execute-api.us-east-1.amazonaws.com/dev',
+        region: 'us-east-1' 
+      }
+  };
