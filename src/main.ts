bbee5d540b4513d85c15d52bfdeacf6c22f460be
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-07-08 11:18:08 +0100

    creation of Amplify resources

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { RootComponent } from './app/components/root/root.component';
 import { Amplify }from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-//Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 bootstrapApplication(RootComponent, appConfig)
   .catch((err) => console.error(err));
