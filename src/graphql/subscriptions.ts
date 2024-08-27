0b7fa3a338b52f806655088c5ecea607fb6e9906
Lomax Osomba <Lomax.Osomba@students.ittralee.ie>
2024-08-27 13:41:56 +0100

    refactor: Update GraphQL import paths to match case-sensitive file names

--- src/graphql/subscriptions.ts
+++ src/graphql/subscriptions.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-import * as APITypes from "../GraphqlAPI";
+import * as APITypes from "../graphqlAPI";
 type GeneratedSubscription<InputType, OutputType> = string & {
   __generatedSubscriptionInput: InputType;
   __generatedSubscriptionOutput: OutputType;
