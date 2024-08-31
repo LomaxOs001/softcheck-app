4806075340b9a8edf21a08f52a1b85d81e6b7663
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-08-31 12:15:51 +0100

    refactor: update root.component.ts

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -3,12 +3,8 @@ import { Router, RouterOutlet, RouterModule } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../../../aws-exports';
 import { Hub } from 'aws-amplify/utils';
-import { fetchAuthSession} from 'aws-amplify/auth';
-import { AuthenticatorService } from '@aws-amplify/ui-angular';
 import { MyLocalStorage } from '../../services/localStorageServices';
 import { CRUDOperations } from '../../models/CRUDOperations';
-import { ProductService } from '../../services/productServices';
-import { ProductDocuments } from '../../models/productDocuments';
 
 Amplify.configure(awsconfig);
 @Component({
@@ -20,9 +16,7 @@ Amplify.configure(awsconfig);
 
 class RootComponent implements OnInit { 
 
-  private crud = new CRUDOperations();
-
-  constructor(private router: Router, private session: MyLocalStorage, private authService: AuthenticatorService, private productService: ProductService) {}
+  constructor(private router: Router, private session: MyLocalStorage) {}
 
   
   ngOnInit(): void {
@@ -37,45 +31,9 @@ class RootComponent implements OnInit {
         this.session.clear();
       }
     });
-
-    this.checkGroupTypes();
     
   }
-  //Return Amazon Cognito group belonging to current authenticated user
-  async getGroups(): Promise<string[]> {
-    try {
-      const session = await fetchAuthSession();
-      const groups = session.tokens?.accessToken?.payload["cognito:groups"] as string[];
-      //console.log("cognito groups: ", groups);
-      return groups || [];
-    } catch (error) {
-      console.error("Error fetching groups: ", error);
-      return [];
-    }
-  }
-  
-  //verify group types to fetch either producer or consumer data
-  async checkGroupTypes() {
-    const groups = await this.getGroups();
-    let id = '';
-  
-    if (groups.includes("producers")) {
- 
-      id = this.authService.user.userId;
-      const result = await this.crud.fetchProductItemsById(id); //fetch to return producers' data via GraphQL query
-      this.productService.updateProductDocumentService(result);
 
-    } else if (groups.includes("consumers")) {
-      
-      const result = await this.crud.fetchProductItems(); //fetch to return consumers' data via GraphQL query
-      this.productService.updateProductDocumentService(result);
-      
-    } else {
-      alert("Product items not found - wrong user group type!");
-      return;
-    }
-  }
-  
 }
 
 export { RootComponent };
