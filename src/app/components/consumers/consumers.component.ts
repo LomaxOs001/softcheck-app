2fd0b6c263a3bd4529fbaf12ce1bf187b2cf3054
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-08-31 12:14:43 +0100

    Update consumers.component.ts

--- src/app/components/consumers/consumers.component.ts
+++ src/app/components/consumers/consumers.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../../../aws-exports';
-import {AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import {AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ProductDocuments } from '../../models/productDocuments';
 import { ProductService } from '../../services/productServices';
 import { VulnerabilityDocuments } from '../../models/vulnerabilityDocuments';
 import { SeverityComponent } from '../severity/severity.component';
+import { UserServices } from '../../services/userServices';
 
 Amplify.configure(awsconfig)
 @Component({
@@ -25,19 +26,35 @@ class ConsumersComponent {
   consumerDocs: ProductDocuments[] = [];
   productVulnerabilityState: VulnerabilityDocuments[] = [];
 
-  constructor(public productService: ProductService ) {}
+  constructor(public productService: ProductService, private authService: AuthenticatorService, private userService: UserServices ) {}
 
   ngOnInit() {
+
+    this.checkGroupTypes();
+
+    //pass new product documents
     this.productService.observeFetchedProductDocuments.subscribe(data => {
       this.consumerDocs = data;
     });
   }
 
+  //ensure it's consumer group prior to fetching data
+  async checkGroupTypes() {
+    const groups = await this.userService.getGroups();
+    let id = '';
+  
+    if (groups.includes("consumers")) {
+      id = this.authService.user.userId;
+      this.productService.fetchProductDocumentsServiceByCollection();
+    }
+    else {
+      alert("Product items not found - wrong user group type!");
+      return;
+    }
+  }
   //fetch to return product's vulnerability state 
   fetchSeverityDetails(stateId: string) {
     this.productService.fetchVulnerabilityDocumentService(stateId);
   }
-  
-
 }
 export {ConsumersComponent}
