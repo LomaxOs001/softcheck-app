2c141314a8183166b1baafdf9dcd63da503645cf
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-08-31 11:40:54 +0100

    Update producers.component.ts

--- src/app/components/producers/producers.component.ts
+++ src/app/components/producers/producers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../../../aws-exports';
-import { AmplifyAuthenticatorModule} from '@aws-amplify/ui-angular';
+import { AmplifyAuthenticatorModule, AuthenticatorService} from '@aws-amplify/ui-angular';
 import { HeaderComponent } from '../header/header.component';
 import { AddproductComponent } from '../addproduct/addproduct.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -10,6 +10,7 @@ import { ProductService } from '../../services/productServices';
 import { ProductDocuments} from '../../models/productDocuments';
 import { VulnerabilityDocuments } from '../../models/vulnerabilityDocuments';
 import { SeverityComponent } from '../severity/severity.component';
+import { RootComponent } from '../root/root.component';
 
 Amplify.configure(awsconfig);
 @Component({
@@ -25,13 +26,35 @@ export class ProducersComponent implements OnInit {
   productDocs: ProductDocuments[] = [];
   productVulnerabilityState: VulnerabilityDocuments[] = [];
 
-  constructor(public productService: ProductService) {}
+  constructor(public productService: ProductService, private authService: AuthenticatorService, private rootComponent: RootComponent) {}
 
   ngOnInit() {
+
+    this.checkGroupTypes();
+
     this.productService.observeFetchedProductDocuments.subscribe(data => {
       this.productDocs = data;
     });
 
+  }
+
+  //fetch product items of a producer user
+  async checkGroupTypes() {
+    const groups = await this.rootComponent.getGroups();
+    let id = '';
+  
+    if (groups.includes("producers")) {
+      id = this.authService.user.userId;
+      this.productService.fetchProductDocumentServiceById(id);
+    }
+    else {
+      alert("Product items not found - wrong user group type!");
+      return;
+    }
+  }
+  //fetch a product's vulnerability state 
+  fetchSeverityDetails(stateId: string) {
+    this.productService.fetchVulnerabilityDocumentService(stateId);
   }
 
   openAddProductModal() {
@@ -39,11 +62,6 @@ export class ProducersComponent implements OnInit {
   }
   closeAddProductModal() {
     this.addProductVisible = false;
-  }
-
-  //fetch to return product's vulnerability state 
-  fetchSeverityDetails(stateId: string) {
-    this.productService.fetchVulnerabilityDocumentService(stateId);
   }
 
 }
