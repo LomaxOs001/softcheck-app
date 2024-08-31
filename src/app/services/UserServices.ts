6a87cf01969c166cc9be14034c24b94b658b4fda
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-08-31 11:52:00 +0100

    Update with a file 'UserServices.ts'

---
+++ src/app/services/UserServices.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { fetchAuthSession} from 'aws-amplify/auth';
+
+@Injectable({
+    providedIn: 'root',
+})
+
+class UserServices {
+    async getGroups(): Promise<string[]> {
+        try {
+          const session = await fetchAuthSession();
+          const groups = session.tokens?.accessToken?.payload["cognito:groups"] as string[];
+          return groups || [];
+        } catch (error) {
+          console.error("Error fetching groups: ", error);
+          return [];
+        }
+      }
+}
+export {UserServices}
