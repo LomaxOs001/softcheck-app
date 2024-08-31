5be70db9ea5c579507de5102e9a8985ea73571a7
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-08-31 12:16:43 +0100

    refactor: update productServices.ts

--- src/app/services/productServices.ts
+++ src/app/services/productServices.ts
@@ -18,24 +18,34 @@ class ProductService {
     observeFetchedProductDocuments = this.productDetails.asObservable();
     observeFetchedVulnerabilityDocuments = this.vulnerabilityDetails.asObservable();
 
-    updateProductDocumentService(data: ProductDocuments[]) {
+    
+    async fetchProductDocumentServiceById(productId: string) {
+        const productDocs = await this.crud.fetchProductItemsById(productId); //fetch producers' data via GraphQL query
+        this.updateProductDocumentService(productDocs);
+    }
+
+    async fetchProductDocumentsServiceByCollection() {
+        const productDocs = await this.crud.fetchProductItems();//fetch consumers' data via GraphQL query
+        this.updateProductDocumentService(productDocs);
+    }
+
+    private updateProductDocumentService(data: ProductDocuments[]) {//update product document data with new fetched product items
         this.productDetails.next(data);
       }
       
     
     async fetchVulnerabilityDocumentService(stateId: string) {
-        const severityList = await this.crud.fetchVulnerabilityDetails(stateId);
+        const severityList = await this.crud.fetchVulnerabilityDetails(stateId);//fetch vulnerability state via GraphQL query
         this.updateVulnerabilityDocumentService(severityList);
         this.showQualitativeRepresentation = true;
     }
 
-    private updateVulnerabilityDocumentService(data: VulnerabilityDocuments[]) {
+    private updateVulnerabilityDocumentService(data: VulnerabilityDocuments[]) {//update vulnerability document data with new fetched vulnerability items
         this.vulnerabilityDetails.next(data);
     }
     closeQualitativeRepresentationModal() {
         this.showQualitativeRepresentation = false; 
       }
-
 
 
 }
