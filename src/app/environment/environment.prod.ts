cd934a7ef080760882c148b1d3ee7ec913508ed9
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-09-07 19:26:35 +0100

    Update environment.prod.ts

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: false,
-  finchainApp: {
-    USER_POOL_ID: "us-east-1_6disTWxvp",
-    USER_POOL_WEB_CLIENT_ID:"6hu9l4m2d0ddvg4kvo2s28dsj3",
-    IDENTITY_POOL_ID:"us-east-1:720fecd2-0a9a-4fc5-aa7a-4ed5d5f37e53",
-      END_POINT_URL:
-        'https://u7jg5lpkgb.execute-api.us-east-1.amazonaws.com/dev',
-      REGION: 'us-east-1' ,
-      BUCKET_NAME: 'finchain_bucket'
-
-    }
+  production: false
 };
